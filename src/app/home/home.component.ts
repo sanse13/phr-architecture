4ce6252df60fedb5679f4a7abacf61662fb59070
Adrian San Segundo <adrisanse.ss@gmail.com>
2024-11-22 16:51:18 +0100

    feat: add scroll down on home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { NgIcon, provideIcons } from '@ng-icons/core';
+import { provideIcons } from '@ng-icons/core';
 import { bootstrapChevronDown } from '@ng-icons/bootstrap-icons';
 import { PopOverComponent } from '../pop-over/pop-over.component';
 
@@ -15,49 +15,21 @@ import { PopOverComponent } from '../pop-over/pop-over.component';
 export class HomeComponent implements AfterViewInit {
   @ViewChild('imageSection') imageSection!: ElementRef;
 
-  prueba(): void {
-    const target = this.imageSection.nativeElement;
-
-    const targetPosition = target.offsetTop;
-
-    const duration = 500;
-    const startPosition = window.pageYOffset;
-    const distance = targetPosition - startPosition;
-    let startTime: number;
-
-    const scrollAnimation = (timestamp: number) => {
-      if (!startTime) startTime = timestamp;
-
-      const timeElapsed = timestamp - startTime;
-      const scrollProgress = Math.min(timeElapsed / duration, 1);
-
-      window.scrollTo(0, startPosition + distance * scrollProgress);
-
-      if (timeElapsed < duration) {
-        requestAnimationFrame(scrollAnimation);
-      }
-    };
-
-    requestAnimationFrame(scrollAnimation);
-  }
-
   ngAfterViewInit(): void {
-    const observer = new IntersectionObserver(
-      (entries, observer) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            entry.target.classList.remove('opacity-0');
-            entry.target.classList.add('opacity-100');
-            observer.unobserve(entry.target);
-          }
-        });
-      },
-      { threshold: 0.5 },
-    );
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.remove('opacity-0');
+          entry.target.classList.add('opacity-100');
+          observer.unobserve(entry.target);
+        }
+      });
+    }, { threshold: 0.1 });
 
     const images = document.querySelectorAll('.fade-in-image');
-    images.forEach((image) => observer.observe(image));
+    images.forEach(image => observer.observe(image));
   }
+
 
   scrollIntoView(): void {
     const target = this.imageSection.nativeElement;
