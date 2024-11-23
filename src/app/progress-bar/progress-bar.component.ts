a0dfdbdd77f66d74ce62391b14b246961bf8564f
Adrian San Segundo <adrisanse.ss@gmail.com>
2024-11-23 14:34:40 +0100

    feat: animate progress bar

--- src/app/progress-bar/progress-bar.component.ts
+++ src/app/progress-bar/progress-bar.component.ts
@@ -1,13 +1,30 @@
-import { Component, input } from '@angular/core';
-import { NgStyle } from '@angular/common';
+import { Component, input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-progress-bar',
   standalone: true,
-  imports: [NgStyle],
+  imports: [],
   templateUrl: './progress-bar.component.html',
   styleUrl: './progress-bar.component.scss',
 })
-export class ProgressBarComponent {
+export class ProgressBarComponent implements OnInit {
   barWidth = input.required<number>();
+  currentWidth = 0;
+
+  ngOnInit(): void {
+    this.animateProgress();
+  }
+
+  animateProgress(): void {
+    const step = 2;
+    const interval = 20;
+
+    const intervalId = setInterval(() => {
+      if (this.currentWidth < this.barWidth()) {
+        this.currentWidth += step;
+      } else {
+        clearInterval(intervalId);
+      }
+    }, interval);
+  }
 }
