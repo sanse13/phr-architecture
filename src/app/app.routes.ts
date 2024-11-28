5bd3e4d0bd6fb27daaefcbdda57f5dfd34a12eea
Adrian San Segundo <adrian@orvium.io>
2024-11-28 21:49:56 +0100

    feat: add contact page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -17,4 +17,8 @@ export const routes: Routes = [
     path: 'gallery/:id',
     component: PhotoGalleryComponent,
   },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
 ];
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {NgIcon, provideIcons} from '@ng-icons/core';
+import {bootstrapLinkedin} from '@ng-icons/bootstrap-icons';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators,} from '@angular/forms';
+
+interface EmailForm {
+  sourceEmail: FormControl<string>;
+  messageBody: FormControl<string>;
+}
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [NgIcon, ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss',
+  viewProviders: [provideIcons({bootstrapLinkedin})],
 })
-export class ContactComponent {}
+export class ContactComponent {
+  contactForm: FormGroup;
+  destinationEmail = 'pablo.hernandez.reques@gmail.com';
+
+  constructor() {
+    this.contactForm = new FormGroup<EmailForm>({
+      sourceEmail: new FormControl<string>('', {
+        nonNullable: true,
+        validators: [Validators.required],
+      }),
+      messageBody: new FormControl<string>('', {
+        nonNullable: true,
+        validators: [Validators.required],
+      }),
+    });
+  }
+
+  sendEmail(): void {
+    const {sourceEmail, messageBody} = this.contactForm.getRawValue();
+
+    window.location.href = `mailto:${this.destinationEmail}?subject=Nuevo%20mensaje%20de%20${encodeURIComponent(sourceEmail)}&body=${encodeURIComponent(messageBody)}`;
+  }
+}
